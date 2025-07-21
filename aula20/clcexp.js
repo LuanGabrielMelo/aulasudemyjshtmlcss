730881056a43fa22525c286e9b8af9ad37d22852
Luan Gabriel <luansantomelo@gmail.com>
2025-07-21 11:12:49 -0300

    exugando o codigo

--- aula20/clcexp.js
+++ aula20/clcexp.js
@@ -16,19 +16,20 @@ let ladoDireito = Number(partes[1].trim());
 *trim() remove espaços desnecessários.
 *Number(...) converte de string para número.
 *Agora ladoDireito = 6.
-let termo = partes[0].trim().replace("x", "").replace("+", "").trim();
+let termo = partes[0].trim().replace("x", "").replace("+", "").trim(); 
 -Pega o lado esquerdo ("x + 2").
 *Remove o "x" com replace("x", "")
 *Remove o "+" com replace("+", "")
 *Usa trim() para limpar os espaços.
-*Fica só com o número 2, que é o termo adicionado a x.
+*Fica só com o número 2, que é o termo adicionado a x. ou let ladoEsquerdo = Number(partes[0].trim().replace(/[x+]/g, "").trim()); maneira mas curta usando regex com [] que conjunta os dois  exugando 
 *let x = ladoDireito - Number(termo);
 console.log("x = " + x);
  */
 let equacao = "x + 2 = 6";
 console.log(`a expressão ${equacao} é execultada da seguinte forma:`);
 let partes = equacao.split("=");
-let ladoEsquerdo = Number (partes[0].trim().replace("x", "").replace("+", "").trim());
+// let ladoEsquerdo = Number (partes[0].trim().replace("x", "").replace("+", "").trim());
+let ladoEsquerdo = Number(partes[0].trim().replace(/[x+]/g, "").trim());
 let ladoDireito = Number(partes[1].trim());
 let x = ladoDireito - ladoEsquerdo;
 console.log(`separa a incógnita da equação "x"  fazendo assim ser  essa resolução das expressões \n x = ${ladoDireito} - ${ladoEsquerdo}\n x = ${x}`);
