94385f598c29a65cfaf3cf7b27728d499add46d0
Luan Gabriel <luansantomelo@gmail.com>
2025-07-21 14:22:34 -0300

    aula 21 alert confirm e propmt

---
+++ aula21/index.js
@@ -0,0 +1,2 @@
+/* o alert não é exatamente um objeto ou função do js mas sim do window(janela do html) que faz surge um aviso  em um bloco  dando um alerta , o objeto em sim é o window  */
+alert('Bem vindo!!')
