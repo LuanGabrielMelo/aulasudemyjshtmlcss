6b54460ba05c873b7bac46b685fa2c8168aec376
Luan Gabriel <luansantomelo@gmail.com>
2025-07-28 12:21:30 -0300

    aula 23 exercicio com variaveis

---
+++ aula22/aula23.js
@@ -0,0 +1,13 @@
+let varA = 'A';//B
+let varB = 'B';//C
+let varC = 'C';//A
+/*const varAtemp = varA;//or let 
+varA=varB;
+varB=varC;
+varC= varAtemp;
+console.log(varA,varB,varC);
+MANEIRA ANTIGA DE SE FAZER 
+*/
+[varA,varB,varC]=[varB,varC,varA]// modernizção do js utilizando metodos matematicos 
+console.log(varA,varB,varC);
+
