00b40948aa19397887e2ff4f5aef3f334978022a
Luan Gabriel <luansantomelo@gmail.com>
2025-07-29 14:26:05 -0300

    as

--- aula21/index.js
+++ aula21/index.js
@@ -13,21 +13,22 @@ console.log(resultado);
 console.log(typeof num1,typeof num2, typeof resultado);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const inserirnome = document.querySelector(".text");
+  const inserirnome = document.querySelectorAll(".text");
   const botao = document.querySelector(".enviar");
   const inputs =document.querySelectorAll("input");
 
-  inserirnome.focus();//focar no input 
+  inserirnome[0].focus();//focar no input 
   //validação e suas tratativas do campo :==>
    botao.addEventListener("click", function () {
-    const nomeDigitado = inserirnome.value; // remove espaços extras
+    const nomeDigitado = inserirnome[0].value; // remove espaços extras
+    const sobrenome =inserirnome[1].value
     const apenasLetras = /^[a-zA-Z]+$/; // Expressão regular para aceitar apenas letras (maiúsculas e minúsculas)
-    if (apenasLetras.test(nomeDigitado)) {
-      console.log("Nome válido:", nomeDigitado);
-      alert("Nome válido:", nomeDigitado);
+    if (apenasLetras.test(nomeDigitado) && apenasLetras.test(sobrenome)) {
+      console.log(`Nome válido:${nomeDigitado}\t${sobrenome}`);
+      alert(`Nome válido:\t${nomeDigitado}\t${sobrenome}`);
     } else {
       console.log("Erro: digite uma letra, não um número ou caractere inválido.");
-      alert('Erro: digite uma letra, não um número ');
+      alert('Erro: digite uma letra, não um número ou caractere inválido.');
     }
   });
   //tratativa para o proximo campo e suas interações com o teclado :==>
@@ -38,7 +39,17 @@ document.addEventListener("DOMContentLoaded", function () {
         input.value = input.value.slice(0, -1);
       }
       if (tecla === "Enter") {
-        event.preventDefault(); // impede submit padrão
+
+        event.preventDefault(); // impede submit padrão 
+
+        const valorDigitado = input.value.trim();
+        const apenasLetras = /^[a-zA-Z]+$/;
+
+         if (!apenasLetras.test(valorDigitado)) {
+         alert("Erro: digite apenas letras, sem números ou espaços.");
+         input.focus(); // mantém o foco no input atual
+          return inserirnome[0]; // impede que avance para o próximo
+           }
         if (index < inputs.length - 1) {
           inputs[index + 1].focus();// vai para o próximo input
         } else {
