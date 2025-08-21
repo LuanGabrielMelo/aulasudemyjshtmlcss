17c1104c9bfb32e9f7be4bae3c5d6606bad28e93
Luan Gabriel <luansantomelo@gmail.com>
2025-08-21 13:14:45 -0300

    subindo a aula27

---
+++ aula27/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.49;
+// let num2 = Math.floor(num1); //aproxima(aredonda o valor ) para baixo
+// let num2 = Math.ceil(num1); // aproxima(aredonda o valor ) para cima
+// let num2 = Math.round(num1); // aproxima(aredonda o valor ) para cima ou para baixo a depender do valo se for, 2,49 para baixo se for 2,50 ai ja vai para cima depende do valor
+//console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 125, 130)); o Math.max faz encontrar o valor maior no campo ou variavel chamada
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 125, 130));// o Math.min faz encontrar o menor valor no campo ou variavel chamada
+// const aleatorio = Math.random() * (10 - 5) + 5;// o random faz gerar um numero aleatorio
+const aleatorio = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+console.log(aleatorio);
